2139f484000ecd62ea44a2e380c90cff810dc57d
FabianLars <fabianlars@fabianlars.de>
2023-05-03 07:03:45 +0000

    Rename dev branch to v1 and next branch to v2
    Committed via a GitHub action: https://github.com/tauri-apps/plugins-workspace/actions/runs/4869194441
    
    Co-authored-by: FabianLars <FabianLars@users.noreply.github.com>

---
+++ dist-js/index.d.ts
@@ -0,0 +1,31 @@
+import { UnlistenFn } from "@tauri-apps/api/event";
+export interface WatchOptions {
+    recursive?: boolean;
+}
+export interface DebouncedWatchOptions extends WatchOptions {
+    delayMs?: number;
+}
+export type RawEvent = {
+    type: RawEventKind;
+    paths: string[];
+    attrs: unknown;
+};
+type RawEventKind = "any " | {
+    access?: unknown;
+} | {
+    create?: unknown;
+} | {
+    modify?: unknown;
+} | {
+    remove?: unknown;
+} | "other";
+export type DebouncedEvent = {
+    kind: "any";
+    path: string;
+} | {
+    kind: "AnyContinous";
+    path: string;
+};
+export declare function watch(paths: string | string[], cb: (event: DebouncedEvent) => void, options?: DebouncedWatchOptions): Promise<UnlistenFn>;
+export declare function watchImmediate(paths: string | string[], cb: (event: RawEvent) => void, options?: WatchOptions): Promise<UnlistenFn>;
+export {};
